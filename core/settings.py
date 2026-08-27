c9e617d3ad0b3fef6955a0cb3a97481c29dbde37
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-27 17:53:06 +0200

    change REST_FRAMEWORK from  'rest_framework.permissions.IsAuthenticatedOrReadOnly' to 'rest_framework.permissions.IsAuthenticated'

--- core/settings.py
+++ core/settings.py
@@ -143,6 +143,6 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
