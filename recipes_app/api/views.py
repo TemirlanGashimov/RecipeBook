92f638a71d56d80928778eddc334f4f12be905da
TemirlanGashimov <gashimov.temerlan@gmail.com>
2026-08-27 17:24:45 +0200

    refactor: Comment out perform_create method in RecipeViewSet

--- recipes_app/api/views.py
+++ recipes_app/api/views.py
@@ -8,5 +8,5 @@ class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = RecipeSerializer
 
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
